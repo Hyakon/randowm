6c016c5c79bbb548974349da491b6cd10e38cae0
hyakon <hyakon@pop-os.localdomain>
2021-01-07 15:35:35 +0100

    first commit

---
+++ script_01.js
@@ -0,0 +1,43 @@
+const entrepreneurs = [
+  { first: "Steve", last: "Jobs", year: 1955 },
+  { first: "Oprah", last: "Winfrey", year: 1954 },
+  { first: "Bill", last: "Gates", year: 1955 },
+  { first: "Sheryl", last: "Sandberg", year: 1969 },
+  { first: "Mark", last: "Zuckerberg", year: 1984 },
+  { first: "Beyonce", last: "Knowles", year: 1981 },
+  { first: "Jeff", last: "Bezos", year: 1964 },
+  { first: "Diane", last: "Hendricks", year: 1947 },
+  { first: "Elon", last: "Musk", year: 1971 },
+  { first: "Marissa", last: "Mayer", year: 1975 },
+  { first: "Walt", last: "Disney", year: 1901 },
+  { first: "Larry", last: "Page", year: 1973 },
+  { first: "Jack", last: "Dorsey", year: 1976 },
+  { first: "Evan", last: "Spiegel", year: 1990 },
+  { first: "Brian", last: "Chesky", year: 1981 },
+  { first: "Travis", last: "Kalanick", year: 1976 },
+  { first: "Marc", last: "Andreessen", year: 1971 },
+  { first: "Peter", last: "Thiel", year: 1967 },
+];
+
+const entrepreneursFullname = entrepreneurs.map((entrepreneur) => {
+  const { first, last } = entrepreneur;
+  return { first, last };
+});
+
+const actualYear = 2020;
+const entrepreneursAge = entrepreneurs.map((entrepreneur) => {
+  let { year: age, ...data } = entrepreneur;
+  age = actualYear - age;
+  return { ...data, age };
+});
+
+const entrepreneursRename = entrepreneurs.map((entrepreneur) => {
+  let { first: firstName, last: lastName, ...data } = entrepreneur;
+  return { firstName, lastName, ...data };
+});
+
+const entrepreneursSenior = entrepreneursAge.filter(
+  (entrepreneur) => entrepreneur.age > 69
+);
+
+console.log(entrepreneursSenior);
